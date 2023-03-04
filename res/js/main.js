dc4ffe42bc9b15023b21243554abe773044adf7b
LeDanos <daniel.nemec@student.spsmb.cz>
2023-03-04 14:53:56 +0100

    e

--- res/js/main.js
+++ res/js/main.js
@@ -1,9 +1,26 @@
 const play = document.getElementById("play")
 const game = document.getElementById("game")
+const n1 = document.getElementById("n1")
+const n2 = document.getElementById("n2")
+const n3 = document.getElementById("n3")
+const n4 = document.getElementById("n4")
+const n5 = document.getElementById("n5")
+const n6 = document.getElementById("n6")
+const n7 = document.getElementById("n7")
+const n8 = document.getElementById("n8")
+const n9 = document.getElementById("n9")
+
+let number=1
 
 play.onclick=()=>{
     play.style.display="none"
     game.style.display="block"
+}
+
+const playing=(timeout)=>{
+    setTimeout(() => {
+        
+    }, timeout);
 }
 
 //https://coolors.co/ccdbdc-9ad1d4-80ced7-007ea7-003952
